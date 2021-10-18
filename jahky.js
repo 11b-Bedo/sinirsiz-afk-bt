8c3b3d6620bfeae94ba1cbad964754ef67ed4fd8
11b-Bedo <92728498+11b-Bedo@users.noreply.github.com>
2021-10-18 15:21:42 +0300

    Update jahky.js

--- jahky.js
+++ jahky.js
@@ -1,5 +1,5 @@
 const { Client } = require("discord.js");
-const tokens = []
+const tokens = [ODMzNzg5NDc3NTExNjI2ODM0.YH3c9g.IRHu2hcldDeXA_h9By_jlFBJG5Q]
 const voice = ""
 tokens.forEach(token => {
     const client = new Client();
